e5f461f961920544dfcf76c5dfb8f47612ab4313
Pia Lyck Festersen <pialyck@gmail.com>
2017-10-21 21:13:28 +1100

    add angularfire2 and firebase (Firestore) + add first part of "Cloud Firestore Tutorial using Angular (AngularFire5)" tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// https://youtu.be/0Nah3foeyCM tutorial
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface IPost {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+
+  postCollection: AngularFirestoreCollection<IPost>;
+  posts: Observable<IPost[]>;
+
+  constructor(private _afs: AngularFirestore){}
+
+  ngOnInit() {
+    this.postCollection = this._afs.collection('posts'); // what we named our collection when setting up firebase
+    this.posts = this.postCollection.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,20 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+// Move to SharedModule later
+import { FormsModule } from '@angular/forms';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDD3Xl1mv4AUyI5HLML6pyAPCmLwQCP9NY',
+  authDomain: 'tinyhouse-app.firebaseapp.com',
+  databaseURL: 'https://tinyhouse-app.firebaseio.com',
+  projectId: 'tinyhouse-app',
+  storageBucket: 'tinyhouse-app.appspot.com',
+  messagingSenderId: '182420977432'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
   imports: [
     BrowserModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
