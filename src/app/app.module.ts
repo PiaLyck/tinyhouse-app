f2913ad24a48f73cdf96ae3c68d3187c0204e3ae
Pia Lyck Festersen <pialyck@gmail.com>
2017-10-21 15:05:53 +1100

    add WelcomeComponent and change AppComponent to use scss instead of css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
