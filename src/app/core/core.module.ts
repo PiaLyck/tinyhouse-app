06bccc775bf3826b0125123737501387140630b6
Pia Lyck Festersen <pialyck@gmail.com>
2017-10-25 16:45:03 +1100

    add comments on CoreModule and SharedModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* CoreModule should have only services and be imported only once in the AppModule.
+
+* Create a CoreModule with providers for the singleton services you load when the application starts.
+* Import CoreModule in the root AppModule only. Never import CoreModule in any other module.
+* Consider making CoreModule a pure services module with no declarations.
+
+*/
+
 @NgModule({
   imports: [
     CommonModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* SharedModule should have anything but services,
+and be imported in all modules that need the shared stuff (which could also be the AppModule)
+
+* Create a SharedModule with the components, directives, and pipes that you use everywhere in your app.
+This module should consist entirely of declarations, most of them exported.
+
+* The SharedModule may re-export other widget modules,
+such as CommonModule, FormsModule, and modules with the UI controls that you use most widely.
+
+* The SharedModule should not have providers for reasons explained previously.
+Nor should any of its imported or re-exported modules have providers.
+If you deviate from this guideline, know what you're doing and why.
+
+* Import the SharedModule in your feature modules,
+both those loaded when the app starts and those you lazy load later.
+
+*/
+
 @NgModule({
   imports: [
     CommonModule
